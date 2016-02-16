5c039d42a9a7d78a3bdb486ef1983f997875bf7e
RnbwTrip <prorock0721@mail.ru>
2016-02-16 14:43:15 +0600

    Added Basic Operation

--- ooh_calc/Form1.Designer.cs
+++ ooh_calc/Form1.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonDivide = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonMultiply = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textAnswer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonDivide
+            // 
+            this.buttonDivide.BackColor = System.Drawing.Color.Tan;
+            this.buttonDivide.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDivide.Location = new System.Drawing.Point(237, 154);
+            this.buttonDivide.Name = "buttonDivide";
+            this.buttonDivide.Size = new System.Drawing.Size(69, 58);
+            this.buttonDivide.TabIndex = 14;
+            this.buttonDivide.Text = "/";
+            this.buttonDivide.UseVisualStyleBackColor = false;
+            this.buttonDivide.Click += new System.EventHandler(this.buttonDivide_Click);
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.BackColor = System.Drawing.Color.Tan;
+            this.buttonPlus.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPlus.Location = new System.Drawing.Point(12, 154);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(69, 58);
+            this.buttonPlus.TabIndex = 13;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = false;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.BackColor = System.Drawing.Color.Tan;
+            this.buttonMinus.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMinus.Location = new System.Drawing.Point(87, 154);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(69, 58);
+            this.buttonMinus.TabIndex = 12;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = false;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // buttonMultiply
+            // 
+            this.buttonMultiply.BackColor = System.Drawing.Color.Tan;
+            this.buttonMultiply.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMultiply.Location = new System.Drawing.Point(162, 154);
+            this.buttonMultiply.Name = "buttonMultiply";
+            this.buttonMultiply.Size = new System.Drawing.Size(69, 58);
+            this.buttonMultiply.TabIndex = 11;
+            this.buttonMultiply.Text = "*";
+            this.buttonMultiply.UseVisualStyleBackColor = false;
+            this.buttonMultiply.Click += new System.EventHandler(this.buttonMultiply_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(293, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(293, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // textAnswer
+            // 
+            this.textAnswer.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textAnswer.Location = new System.Drawing.Point(13, 112);
+            this.textAnswer.Name = "textAnswer";
+            this.textAnswer.ReadOnly = true;
+            this.textAnswer.Size = new System.Drawing.Size(293, 20);
+            this.textAnswer.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 459);
+            this.Controls.Add(this.textAnswer);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonDivide);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonMultiply);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonDivide;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonMultiply;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textAnswer;
     }
 }
 
--- ooh_calc/Form1.cs
+++ ooh_calc/Form1.cs
@@ -16,5 +16,89 @@ namespace ooh_calc
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            String firstString = textBox1.Text;
+            String secondString = textBox2.Text;
+            Double first = Convert.ToDouble(firstString);
+            Double second = Convert.ToDouble(secondString);
+            Double answer = first + second;
+            String answerString = Convert.ToString(answer);
+            textAnswer.Text = answerString;
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            String firstString = textBox1.Text;
+            String secondString = textBox2.Text;
+            Double first = Convert.ToDouble(firstString);
+            Double second = Convert.ToDouble(secondString);
+            Double answer = first - second;
+            String answerString = Convert.ToString(answer);
+            textAnswer.Text = answerString;
+        }
+
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            String firstString = textBox1.Text;
+            String secondString = textBox2.Text;
+            Double first = Convert.ToDouble(firstString);
+            Double second = Convert.ToDouble(secondString);
+            Double answer = first * second;
+            String answerString = Convert.ToString(answer);
+            textAnswer.Text = answerString;
+        }
+
+        private void buttonDivide_Click(object sender, EventArgs e)
+        {
+            String firstString = textBox1.Text;
+            String secondString = textBox2.Text;
+            Double first = Convert.ToDouble(firstString);
+            Double second = Convert.ToDouble(secondString);
+            Double answer = first / second;
+            String answerString = Convert.ToString(answer);
+            textAnswer.Text = answerString;
+        }
     }
 }
